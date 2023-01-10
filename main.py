917c84f0a0b110e53bfbe9e827458e1e294e3084
Danila 1337 <saf1337@DESKTOP-G4LPVNB.localdomain>
2023-01-10 18:31:20 +0300

    Создание контроллера

--- main.py
+++ main.py
@@ -2,11 +2,33 @@ from random import random
 from request_and_response import Response, Request
 
 
-def controller(request: Request, timeout=int(random() * 11)):
-    r = request(url='http://url.com', method='GET', params={'c': 2, 'm': 1}, timeout=4, status=200)
-    try:
-        r.url
-        return 'Done'
-    except:
-        return 'ne Done'
-print(controller(Request))
+def func(url, method, params, status, status_text, content=''):
+    return Response(url=url, method=method, params=params,
+                    status=status, status_text=status_text, content=content)
+
+
+def controller(request: Request, timeout=int(random() * 11)) -> Response:
+    response_dict = {'url': request.url,
+                     'method': request.method,
+                     'params': request.params,
+                     'status': request.status
+                     }
+    if request.method == 'GET' and request.status == 200:
+        response_dict['status_text'] = 'OK'
+        response_dict['content'] = 'OK'
+        return func(**response_dict)
+    elif request.method == 'POST' and request.status == 201:
+        response_dict['status_text'] = 'CREATED'
+        response_dict['content'] = 'CREATED'
+        return func(**response_dict)
+    elif request.status == 400:
+        response_dict['status_text'] = 'BAD_REQUEST'
+        return func(**response_dict)
+    elif request.status == 401:
+        response_dict['status_text'] = 'NOT_AUTH'
+        return func(**response_dict)
+    elif request.status == 404:
+        response_dict['status_text'] = 'NOT_FOUND'
+        return func(**response_dict)
+    else:
+        return 'Неизветсная ошибка'
--- request_and_response/create_response.py
+++ request_and_response/create_response.py
@@ -2,7 +2,8 @@ class Response:
     """
     Создание response для клиента
     """
-    def __init__(self, url: str, method: str, params: dict, status: int, status_text: str, content: str):
+    def __init__(self, url: str, method: str, params: dict, status: int,
+                 status_text: str, content: str = ''):
         self.url = url
         self.method = method
         self.params = params
