09f51f28b49a592ee73aa5907444a36dce4bfe58
Danila 1337 <saf1337@DESKTOP-G4LPVNB.localdomain>
2023-01-10 19:20:23 +0300

    Исправление ошибок и добавление ошибки с таймаутом

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-from random import random
 from request_and_response import Response, Request
 
 
-def func(url, method, params, status, status_text, content=''):
+def create_response(url, method, params, status, status_text, content=''):
+    """
+    Создание Response
+    """
     return Response(url=url, method=method, params=params,
                     status=status, status_text=status_text, content=content)
 
 
-def controller(request: Request, timeout=int(random() * 11)) -> Response:
+def controller(request: Request) -> Response:
+    """
+    Проверка данных для создания Response
+    """
     response_dict = {'url': request.url,
                      'method': request.method,
                      'params': request.params,
                      'status': request.status
                      }
-    if request.method == 'GET' and request.status == 200:
-        response_dict['status_text'] = 'OK'
-        response_dict['content'] = 'OK'
-        return func(**response_dict)
-    elif request.method == 'POST' and request.status == 201:
-        response_dict['status_text'] = 'CREATED'
-        response_dict['content'] = 'CREATED'
-        return func(**response_dict)
-    elif request.status == 400:
-        response_dict['status_text'] = 'BAD_REQUEST'
-        return func(**response_dict)
-    elif request.status == 401:
-        response_dict['status_text'] = 'NOT_AUTH'
-        return func(**response_dict)
-    elif request.status == 404:
-        response_dict['status_text'] = 'NOT_FOUND'
-        return func(**response_dict)
-    else:
-        return 'Ошибка'
+    status_num = {200: 'OK', 201: 'CREATED', 400: 'BAD_REQUEST',
+                  401: 'NOT_AUTH', 404: 'NOT_FOUND'}
+    try:
+        request.timeout
+        if request.method in ['GET', 'POST'] and 300 < request.status >= 200:
+            response_dict['status_text'] = status_num[request.status]
+            response_dict['content'] = status_num[request.status]
+            return create_response(**response_dict)
+        elif 500 < request.status >= 400:
+            response_dict['status_text'] = status_num[request.status]
+            return create_response(**response_dict)
+        else:
+            return 'Ошибка'
+    except:
+        response_dict['status_text'] = 'TIMEOUT_ERROR'
+        response_dict['status'] = 408
+        return create_response(**response_dict)
