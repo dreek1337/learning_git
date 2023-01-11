5414c52df15c16890bb7616a9e1ab29ea4de0e1d
Danila 1337 <saf1337@DESKTOP-G4LPVNB.localdomain>
2023-01-11 19:34:58 +0300

    Новое решение

--- main.py
+++ main.py
@@ -37,16 +37,32 @@ def controller(request: Request) -> Response:
     """
     Проверка данных для создания Response
     """
-    r = request(url='https;//url.com', method='GET', params={str(i): i for i in range(choice(range(7)))},
-                status=choice(range(150, 350, 10)), timeout=choice(range(11)))
-    response_dict = {'url': r.url,
-                     'method': r.method,
-                     'status': r.status
+    response_dict = {'url': request.url,
+                     'method': request.method,
+                     'status': request.status
                      }
     try:
-        r.timeout
-        r.params
-        response_dict['params'] = r.params
+        request.timeout
+        response_dict['params'] = request.params
         return create_response(**response_dict)
     except Exception as err:
         raise f'Ошибочкаааа: {err} | status: 408, status_text: TIMEOUT_ERROR'
+
+
+create_request = [Request(url='https;//url.com', method=choice(('GET', 'POST')),
+                          params={str(i): i for i in range(choice(range(7)))},
+                          status=choice(range(150, 350, 10)), timeout=choice(range(11)))
+                  for i in range(10)]
+
+correct_response = None
+
+for i in create_request:
+    c = controller(i)
+    if c:
+        correct_response = c
+    else:
+        continue
+print(create_request[0].timeout, create_request[1].timeout, create_request[2].timeout)
+print(correct_response.status)
+print(correct_response.method)
+print(correct_response.url)
